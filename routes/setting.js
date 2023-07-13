7d87dd4e9d59a05133eeb1287ad76e6be5d9a7b3
AhmaDev <ahmadwig@gmail.com>
2023-07-13 16:16:39 +0300

    fix

--- routes/setting.js
+++ routes/setting.js
@@ -27,6 +27,27 @@ router.get(`/${tableName}/:id`, function (req, res, next) {
   );
 });
 
+router.get("/reports", function (req, res) {
+  connection.query(
+    `SELECT 
+(SELECT COUNT(idUser) FROM user WHERE roleId = 2 AND createdAt BETWEEN '2023-01-01 00:00:00' AND '2023-09-01 23:59:59') AS employees,
+(SELECT COUNT(idUser) FROM user WHERE roleId = 3 AND createdAt BETWEEN '2023-01-01 00:00:00' AND '2023-09-01 23:59:59') AS companies,
+(SELECT COUNT(idJob) FROM job WHERE createdAt BETWEEN '2023-01-01 00:00:00' AND '2023-09-01 23:59:59') AS jobs,
+(SELECT COUNT(idJobAd) FROM jobAd WHERE status = 'active' AND adTypeId = 1) AS totalJobSlider,
+(SELECT COUNT(idJobAd) FROM jobAd WHERE status = 'active' AND adTypeId = 2) AS totalJobSplash,
+(SELECT COUNT(idJobAd) FROM jobAd WHERE status = 'active' AND adTypeId = 3) AS totalJobSearch,
+(SELECT COUNT(idJobAd) FROM jobAd WHERE status = 'approved') AS approvedAds,
+(SELECT COUNT(idJobAd) FROM jobAd WHERE status = 'pending') AS pendingAds,
+(SELECT COUNT(idJobApplication) FROM jobApplication WHERE status = 'pending') AS pendingApplications,
+(SELECT COUNT(idJobApplication) FROM jobApplication WHERE status = 'approved') AS approvedApplications,
+(SELECT COUNT(idJobApplication) FROM jobApplication WHERE status = 'declined') AS declinedApplications,
+(SELECT COUNT(idCvAd) FROM cvAd WHERE status = 'active') AS activeCvAds`,
+    (err, result) => {
+      res.send(result);
+    },
+  );
+});
+
 router.post(`/add${capitalize(tableName)}`, function (req, res, next) {
   connection.query(
     `INSERT INTO ${tableName} SET ?`,
